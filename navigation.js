e7557635fd29d1894e618056719759fa0af68502
Antti <anlehton24@students.oamk.fi>
2026-02-18 00:51:54 +0200

    Project setup and started navigation file

---
+++ navigation.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import LoginView from "../views/LoginView";
+import LocationsView from "../views/LocationsView";
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Locations" component={LocationsView} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
