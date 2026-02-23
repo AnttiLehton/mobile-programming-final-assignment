419f83c4a0b4d6fcac562e92301993f0f7051da9
Antti <anlehton24@students.oamk.fi>
2026-02-23 20:50:28 +0200

    Made addlocation.js

--- AddLocation.js
+++ AddLocation.js
@@ -1,1 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
+import { View, TextInput, Button } from "react-native";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "./firebase";
+
+export default function AddLocation() {
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const [rating, setRating] = useState("");
+
+    const savelocation = async () => {
+        try {
+            await addDoc(collection(db, "locations"), {
+                name,
+                description,
+                rating,
+            });
+        } catch (error) {
+            console.log(error)
+        }
+    }:
+
+
+ return(
+    <View>
+        <TextInput placeholder="Name" value={name} onChangeText={setName} />
+        <TextInput placeholder="Description" value={description} onChangeText={setDescription} />
+         <TextInput placeholder="Rating" value={rating} onChangeText={setRating} keyboardType="numeric" />
+        <Button title = "Save Location" onPress={savelocation} />
+        </View>
+);
+    }
