edf2b26a1d7bc2d8a496f541c86e6b69b07b0a94
Antti <anlehton24@students.oamk.fi>
2026-02-23 19:15:37 +0200

    added usercontext

--- MainPage.js
+++ MainPage.js
@@ -1,52 +1,39 @@
-import React from "react";
+import React, { useContext } from "react";   
 import { View, Text, StyleSheet, Button } from "react-native";
-import {auth} from "./firebase";
-
+import { auth } from "./firebase";
 import { signOut } from "firebase/auth";
+import { UserContext } from "./navigation";
 
 export default function MainPage({ navigation }) {
-    const user = auth.currentUser;
 
-     const handleLogout = async () => {
-        await signOut(auth);
-        navigation.replace("Login");
-    };
+  const { user } = useContext(UserContext);  
 
+  const handleLogout = async () => {
+    await signOut(auth);
+    navigation.replace("Login");
+  };
 
-return (
+  return (
     <View style={styles.container}>
-        <Text style={styles.title}>Welcome!</Text>
+      <Text style={styles.title}>Tervetuloa!</Text>
 
-        <Button
-         title= "Sijainti"
-            onPress={() => navigation.navigate("Location")}
-        />
+      <Text>{user ? user : "Guest"}</Text>
 
-        <Button
-         title = "Kartta"
-            onPress={() => navigation.navigate("Map")}
-        />
+      <Button title="Sijainti" onPress={() => navigation.navigate("Location")} />
+      <Button title="Kartta" onPress={() => navigation.navigate("Map")} />
+      <Button title="Etsi sijainteja" onPress={() => navigation.navigate("AddLocation")} />
 
-        <Button
-         title = "Etsi sijainteja"
-            onPress={() => navigation.navigate("AddLocation")}
-        />
-
-        <View style={{ marginTop: 20 }}>
-            <Button title="Logout" onPress={handleLogout} color="red"/>
-         </View>
-        </View>
-
-
-);
-
+      <View style={{ marginTop: 20 }}>
+        <Button title="Logout" onPress={handleLogout} color="red" />
+      </View>
+    </View>
+  );
 }
 
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
--- navigation.js
+++ navigation.js
@@ -1,24 +1,30 @@
-import React from "react";
+
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
+import { createContext, useState } from "react";
 
 
 
 import LoginView from "./LoginView";
 import MainPage from "./MainPage";
 
+export const UserContext = createContext(null);
+
 
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigator() {
+  const [user, setUser] = useState(null);
   return (
+    <UserContext.Provider value={{user, setUser}}>
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
       <Stack.Screen name="Login" component={LoginView} />
       <Stack.Screen name="Main" component={MainPage} />
 
+
       </Stack.Navigator>
     </NavigationContainer>
+    </UserContext.Provider>
   );
 }
