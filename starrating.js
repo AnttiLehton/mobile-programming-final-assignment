d3b1e4bd3cac877e5cff2dcfe0ddc8bba0a371de
mimosania <stuomiko24@students.oamk.fi>
2026-03-26 02:00:44 +0200

    added firebase

---
+++ starrating.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAMnIDLgwO2hKNUxVj5Kv45bbBAIH3IQ8U",
+  authDomain: "rating-b2a38.firebaseapp.com",
+  databaseURL: "https://rating-b2a38-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "rating-b2a38",
+  storageBucket: "rating-b2a38.firebasestorage.app",
+  messagingSenderId: "887410091185",
+  appId: "1:887410091185:web:d7dd01b706f0c6730a6e90"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
