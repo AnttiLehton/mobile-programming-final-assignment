09c19aa58ca966dc979b6efd23947f297d99742c
anttilehton <anlehton24@students.oamk.fi>
2026-04-13 19:44:47 +0300

    Trying to fix filters part2

--- CountrySearch.js
+++ CountrySearch.js
@@ -36,29 +36,45 @@ return matchesRegion && matchesPopulation;
 });
 
 
-    return (
-        <View style={[styles.container, {backgroundColor: '#ffe3ef'}]}>
-            <TextInput
-                placeholder='Search country'
-                value={keyword}
-                onChangeText={setKeyword}
-                style={styles.input}
-            /> 
-            <Button color="rgb(59, 168, 99)" title='Search' onPress={searchCountries} />
+return (
+    <View style={[styles.container, {backgroundColor: '#ffe3ef'}]}>
+        <TextInput
+            placeholder='Search country'
+            value={keyword}
+            onChangeText={setKeyword}
+            style={styles.input}
+        /> 
 
-            <FlatList
-                data={filteredResults}
-                keyExtractor={(item) => item.cca3}
-                renderItem={({ item }) => (
-                    <View style={styles.item}>
-                        <Image
-                            source={{ uri: item.flags.png }}
-                            style={{ width: 50, height: 30, marginRight: 10 }}
-                        />  
-                        <Text>{item.name.common}</Text>
-                        <Text> {item.region}</Text>
-                        <Text>population : {item.population}</Text>
-                    </View>
+        <TextInput
+            placeholder='Filter by region'
+            value={regionFilter}
+            onChangeText={setRegionFilter}
+            style={styles.input}
+        />
+
+        <TextInput
+            placeholder='Minimum population'
+            value={minPopulation}
+            onChangeText={setMinPopulation}
+            keyboardType='numeric'
+            style={styles.input}
+        />
+
+        <Button color="rgb(59, 168, 99)" title='Search' onPress={searchCountries} />
+
+        <FlatList
+            data={filteredResults}
+            keyExtractor={(item) => item.cca3}
+            renderItem={({ item }) => (
+                <View style={styles.item}>
+                    <Image
+                        source={{ uri: item.flags.png }}
+                        style={{ width: 50, height: 30, marginRight: 10 }}
+                    />  
+                    <Text>{item.name.common}</Text>
+                    <Text> {item.region}</Text>
+                    <Text>population : {item.population}</Text>
+                </View>
                 )}
             />
         </View>
